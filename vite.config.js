55b549b7103c1632091c5e1f1b6d3ee2378b7ea8
fhf <710790342@qq.com>
2024-12-23 11:57:43 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	base: "/fhf-portfolio",
-	build: {
-		outDir: "dist", // 输出目录
-		rollupOptions: {
-			input: 'src/main.jsx', // 确保这里是正确的入口文件路径
-		},
-	},
 });
